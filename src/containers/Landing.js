67758ded393a05042c1ab07df1d56413edbda8bb
Parker <parker@Parkers-MacBook-Pro-2.local>
2017-03-20 20:01:16 -0700

    FLIP DEM COINZ

--- src/containers/Landing.js
+++ src/containers/Landing.js
@@ -18,17 +18,29 @@ const SubtractCoin = MKButton.coloredButton()
   .withText('Subtract coin')
   .build();
 
+const FlipAllCoins = MKButton.coloredButton()
+  .withText('Flip all coins')
+  .build();
+
 @observer
 class Landing extends Component {
   render() {
     console.log('in landing ', this.props.store.numberOfFlips)
     return (
       <View style={styles.container}>
+        <Text> Individual and group settings </Text>
         <AddCoin onPress={this.props.store.increaseNumberOfFlips} />
         <Text>
           {this.props.store.numberOfFlips}
         </Text>
         <SubtractCoin onPress={this.props.store.decreaseNumberOfFlips} />
+        <FlipAllCoins onPress={this.props.store.flipAllCoins} />
+        <Text>
+          Tails: {this.props.store.flipResultsTails}
+        </Text>
+        <Text>
+          Heads: {this.props.store.flipResultsHeads}
+        </Text>
       </View>
     );
   }
--- src/flipCoin.js
+++ src/flipCoin.js
@@ -3,15 +3,22 @@ import {
   HEADS,
   TAILS
 } from './_constants/coinFlipConstants';
-
+// TODO: decouple. Mixes abstractions for a coin flip and a coin
 class coinFlip {
 
   constructor() {
     this.increaseNumberOfFlips = this.increaseNumberOfFlips.bind(this);
     this.decreaseNumberOfFlips = this.decreaseNumberOfFlips.bind(this);
+    this.flipAllCoins = this.flipAllCoins.bind(this);
+    this.recursiveFlip = this.recursiveFlip.bind(this);
+    this.isHeads = this.isHeads.bind(this);
+    this.isTails = this.isTails.bind(this);
+    this.generateCoinFlip = this.generateCoinFlip.bind(this);
   }
 
   @observable numberOfFlips = 0;
+  @observable flipResultsHeads = 0;
+  @observable flipResultsTails = 0;
 
   increaseNumberOfFlips() {
     this.numberOfFlips += 1;
@@ -23,11 +30,38 @@ class coinFlip {
     console.log(this.numberOfFlips)
   }
 
-  // Returns true for heads, false for tails
+  flipAllCoins() {
+    this.flipResultsHeads = 0;
+    this.flipResultsTails = 0;
+    this.recursiveFlip(this.numberOfFlips);
+  }
+
+  recursiveFlip(flipsRemaining) {
+    if (flipsRemaining === 0) return;
+
+    let flipResult = this.generateCoinFlip();
+    if (this.isHeads()) {
+      this.flipResultsHeads += 1;
+    } else { // Case: we have tails
+      this.flipResultsTails += 1;
+    }
+    flipsRemaining -= 1;
+    this.recursiveFlip(flipsRemaining);
+  }
+
   generateCoinFlip() {
-    let coinFlip = Math.floor(Math.random() * 100);
-    if (coinFlip > 50) return HEADS;
-    else return TAILS;
+    this.currentFlip = Math.floor(Math.random() * 100);
+    return this.currentFlip;
+  }
+
+  isHeads() {
+    if (this.currentFlip >= 50) return true;
+    else return false;
+  }
+
+  isTails() {
+    if (this.currentFlip < 50) return true;
+    else return false;
   }
 }
 

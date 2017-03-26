e5564553a903939525effa71a77d18b5b9c1d918
Parker <parker@Parkers-MacBook-Pro-2.local>
2017-03-26 15:35:31 -0700

    rework UI and animate new additional to flip history

--- src/containers/Landing.js
+++ src/containers/Landing.js
@@ -11,14 +11,13 @@ import {
 import { MKButton } from 'react-native-material-kit';
 import SpinningCoin from '../components/SpinningCoin';
 import {Surface} from 'gl-react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 // colored button with default theme (configurable)
-const AddCoin = MKButton.coloredButton()
-  .withText('Add coin')
+const AddCoin = MKButton.coloredFab()
   .build();
 
-const SubtractCoin = MKButton.coloredButton()
-  .withText('Subtract coin')
+const SubtractCoin = MKButton.coloredFab()
   .build();
 
 const FlipAllCoins = MKButton.coloredButton()
@@ -50,36 +49,49 @@ class Landing extends Component {
     console.log('in landing ', this.props.store.numberOfFlips)
     return (
       <View style={styles.container}>
-        <AddCoin onPress={this.props.store.increaseNumberOfFlips} />
-        <View style={{justifyContent : 'center', alignItems : 'center', marginVertical : 20}}>
-          <Text>
-            {this.props.store.numberOfFlips}
-          </Text>
+        <View style={{ flex : 1 }}>
+          <View style={{flexDirection : 'row', alignItems : 'center', justifyContent : 'center'}}>
+            <AddCoin onPress={this.props.store.increaseNumberOfFlips}>
+              <Icon name="plus" style={{color : 'white', fontSize : 20}}/>
+            </AddCoin>
+            <View style={{ marginHorizontal : 20}}>
+              <Text>
+                {this.props.store.numberOfFlips}
+              </Text>
+            </View>
+            <SubtractCoin onPress={this.props.store.decreaseNumberOfFlips}>
+              <Icon name="minus" style={{color : 'white', fontSize : 20}}/>
+            </SubtractCoin>
+          </View>
+          {/*<Surface width={300} height={200}>
+            <SpinningCoin />
+          </Surface>*/}
+          <View style={{flexDirection:'row', alignItems : 'center', justifyContent : 'space-between'}}>
+            <FlipAllCoins onPress={() => {
+                this._appendFlipResult(this.props.store.flipAllCoins());
+              }} />
+            <FlipOne onPress={() => {
+                this._appendFlipResult(this.props.store.flipSingleCoin());
+              }} />
+          </View>
         </View>
-        <SubtractCoin onPress={this.props.store.decreaseNumberOfFlips} />
-        {/*<Surface width={300} height={200}>
-          <SpinningCoin />
-        </Surface>*/}
-        <View style={{flex : 1, flexDirection:'row', alignItems : 'center', justifyContent : 'space-between'}}>
-          <FlipAllCoins onPress={() => {
-              this._appendFlipResult(this.props.store.flipAllCoins());
-            }} />
-          <FlipOne onPress={() => {
-              this._appendFlipResult(this.props.store.flipSingleCoin());
-            }} />
-        </View>
-        <ScrollView>
+        <ScrollView style={{ flexGrow : 2, borderTopWidth : 0.5 }} ref='flipResults'>
           {this.state.pastFlips.map((flipResults, i) => {
             return (
-              <View key={i} style={{paddingVertical : 10, borderBottomWidth : 1}}>
+              <View onLayout={(event) => {
+                  if (i === this.state.pastFlips.length - 1) {
+                    this.refs.flipResults.scrollTo({ y : event.nativeEvent.layout.height, animated : false});
+                    this.refs.flipResults.scrollTo({ y : 0, animated : true});
+                  }
+                }} key={i} style={{paddingVertical : 10, borderBottomWidth : 1}}>
                 <Text style={{fontWeight : 'bold'}}>
                   Flip {i + 1}
                 </Text>
                 <Text>
-                  Tails: {flipResults.tails}
+                  T: {flipResults.tails}
                 </Text>
                 <Text>
-                  Heads: {flipResults.heads}
+                  H: {flipResults.heads}
                 </Text>
                 <Text>
                   Total coins: {flipResults.heads + flipResults.tails}
@@ -96,6 +108,7 @@ class Landing extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent : 'flex-start',
     borderBottomWidth: StyleSheet.hairlineWidth,
     ...StyleSheet.absoluteFillObject
   },
